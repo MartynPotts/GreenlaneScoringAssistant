17001d789760fdf7ab0be52e914df4dd8dfebe68
Martyn Potts <w19005228@northumbria.ac.uk>
2023-04-03 14:54:53 +0100

    Archer details page started

---
+++ app/src/main/java/com/martynpotts/greenlanescoringassistant/ScoreRound.java
@@ -0,0 +1,14 @@
+package com.martynpotts.greenlanescoringassistant;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScoreRound extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_round);
+    }
+}
