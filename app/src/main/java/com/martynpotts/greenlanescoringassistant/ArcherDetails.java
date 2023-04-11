927769dd67d0dd6b6dc19ca006f83d25e8c9163c
Martyn Potts <w19005228@northumbria.ac.uk>
2023-04-11 18:50:19 +0100

    Both profile pages done

--- app/src/main/java/com/martynpotts/greenlanescoringassistant/ArcherDetails.java
+++ app/src/main/java/com/martynpotts/greenlanescoringassistant/ArcherDetails.java
@@ -2,7 +2,17 @@ package com.martynpotts.greenlanescoringassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.martynpotts.greenlanescoringassistant.databinding.ActivityArcherDetailsBinding;
 
@@ -10,6 +20,11 @@ public class ArcherDetails extends DrawerBaseActivity {
 
 
     ActivityArcherDetailsBinding activityArcherDetailsBinding;
+    private EditText archerName, archerAge, gender;
+    private CheckBox archer1st,archer2nd,archer3rd,bowman1st,bowman2nd,bowman3rd,masterBowman,
+            grandMasterBowman,eliteMasterBowman;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +32,103 @@ public class ArcherDetails extends DrawerBaseActivity {
         activityArcherDetailsBinding = ActivityArcherDetailsBinding.inflate(getLayoutInflater());
         setContentView(activityArcherDetailsBinding.getRoot());
         allocateActivityTitle("Archer Details");
+
+        archerName = findViewById(R.id.edtArcherName);
+        archerAge = findViewById(R.id.edtAge);
+        gender = findViewById(R.id.edtGender);
+
+
+        archer1st = findViewById(R.id.chkArcherFirst);
+        archer2nd = findViewById(R.id.chkArcherSecond);
+        archer3rd = findViewById(R.id.chkArcherThird);
+
+        bowman1st = findViewById(R.id.chkBowmanFirst);
+        bowman2nd = findViewById(R.id.chkBowmanSecond);
+        bowman3rd = findViewById(R.id.chkBowmanThird);
+
+        masterBowman = findViewById(R.id.chkMasterBowman);
+        grandMasterBowman = findViewById(R.id.chkGrandMasterBowman);
+        eliteMasterBowman = findViewById(R.id.chkEliteMasterBowman);
+
     }
+
+    public void savePreferences(){
+        SharedPreferences sp = getSharedPreferences("SharedPreferences",MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sp.edit();
+
+        myEdit.putString("Name",archerName.getText().toString() );
+        try {
+            myEdit.putInt("Age", Integer.parseInt(archerAge.getText().toString()));
+        } catch (NumberFormatException nfe){
+            myEdit.putInt("Age", 0);
+        }
+        myEdit.putString("Gender",gender.getText().toString());
+
+        myEdit.putBoolean("Archer1st",archer1st.isChecked());
+        myEdit.putBoolean("Archer2nd",archer2nd.isChecked());
+        myEdit.putBoolean("Archer3rd",archer3rd.isChecked());
+
+        myEdit.putBoolean("Bowman1st",bowman1st.isChecked());
+        myEdit.putBoolean("Bowman2nd",bowman2nd.isChecked());
+        myEdit.putBoolean("Bowman3rd",bowman3rd.isChecked());
+
+        myEdit.putBoolean("MasterBowman",masterBowman.isChecked());
+        myEdit.putBoolean("GrandMasterBowman",grandMasterBowman.isChecked());
+        myEdit.putBoolean("EliteMasterBowman",eliteMasterBowman.isChecked());
+        myEdit.apply();
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sp = getSharedPreferences("SharedPreferences", MODE_PRIVATE);
+        String nameString = sp.getString("Name","");
+        int ageInt = sp.getInt("Age", 0);
+        String genderString = sp.getString("Gender","");
+
+        archerName.setText(nameString);
+        archerAge.setText(String.valueOf(ageInt));
+        gender.setText(genderString);
+
+        if(sp.contains("Archer1st") && sp.getBoolean("Archer1st",true)==true){
+            archer1st.setChecked(true);
+        }
+        if(sp.contains("Archer2nd") && sp.getBoolean("Archer2nd",true)==true){
+            archer2nd.setChecked(true);
+        }
+        if(sp.contains("Archer3rd") && sp.getBoolean("Archer3rd",true)==true){
+            archer3rd.setChecked(true);
+        }
+
+        if(sp.contains("Bowman1st") && sp.getBoolean("Bowman1st",true)==true){
+            bowman1st.setChecked(true);
+        }
+        if(sp.contains("Bowman2nd") && sp.getBoolean("Bowman2nd",true)==true){
+            bowman2nd.setChecked(true);
+        }
+        if(sp.contains("Bowman3rd") && sp.getBoolean("Bowman3rd",true)==true){
+            bowman3rd.setChecked(true);
+        }
+
+        if(sp.contains("MasterBowman") && sp.getBoolean("MasterBowman",true)==true){
+            masterBowman.setChecked(true);
+        }
+        if(sp.contains("GrandMasterBowman") && sp.getBoolean("GrandMasterBowman",true)==true){
+            grandMasterBowman.setChecked(true);
+        }
+        if(sp.contains("EliteMasterBowman") && sp.getBoolean("EliteMasterBowman",true)==true){
+            eliteMasterBowman.setChecked(true);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        savePreferences();
+    }
+
+
 }
--- app/src/main/java/com/martynpotts/greenlanescoringassistant/BowDetails.java
+++ app/src/main/java/com/martynpotts/greenlanescoringassistant/BowDetails.java
@@ -1,8 +1,8 @@
 package com.martynpotts.greenlanescoringassistant;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.martynpotts.greenlanescoringassistant.databinding.ActivityBowDetailsBinding;
 
@@ -10,11 +10,118 @@ public class BowDetails extends DrawerBaseActivity {
 
     ActivityBowDetailsBinding activityBowDetailsBinding;
 
+    private EditText bowType, poundage, bracingHeight, sightMark20yrds, sightMark30yrds
+            , sightMark40yrds, sightMark50yrds, sightMark60yrds, sightMark80yrds
+            , sightMark100yrds, sightMark30mtrs, sightMark50mtrs, sightMark70mtrs
+            , sightMark90mtrs;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activityBowDetailsBinding = ActivityBowDetailsBinding.inflate(getLayoutInflater());
         setContentView(activityBowDetailsBinding.getRoot());
         allocateActivityTitle("Bow Details");
+
+        bowType = findViewById(R.id.edtBowType);
+        poundage = findViewById(R.id.edtPoundage);
+        bracingHeight = findViewById(R.id.edtBracingHeight);
+
+        sightMark20yrds = findViewById(R.id.edt20YardsSightMark);
+        sightMark30yrds = findViewById(R.id.edt30YardsSightMark);
+        sightMark40yrds = findViewById(R.id.edt40YardsSightMark);
+        sightMark50yrds = findViewById(R.id.edt50YardsSightMark);
+        sightMark60yrds = findViewById(R.id.edt60YardsSightMark);
+        sightMark80yrds = findViewById(R.id.edt80YardsSightMark);
+        sightMark100yrds = findViewById(R.id.edt100YardsSightMark);
+
+        sightMark30mtrs = findViewById(R.id.edt30MetreSightMark);
+        sightMark50mtrs = findViewById(R.id.edt50MetreSightMark);
+        sightMark70mtrs = findViewById(R.id.edt70MetreSightMark);
+        sightMark90mtrs = findViewById(R.id.edt90MetreSightMark);
+    }
+
+    public void savePreferences(){
+        SharedPreferences sp = getSharedPreferences("SharedPreferences",MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sp.edit();
+
+        myEdit.putString("BowType", bowType.getText().toString() );
+        try {
+            myEdit.putInt("Poundage", Integer.parseInt(poundage.getText().toString()));
+
+            myEdit.putFloat("BracingHeight", Float.parseFloat(bracingHeight.getText().toString()));
+
+            myEdit.putFloat("SightMarks20yrds", Float.parseFloat(sightMark20yrds.getText().toString()));
+            myEdit.putFloat("SightMarks30yrds", Float.parseFloat(sightMark30yrds.getText().toString()));
+            myEdit.putFloat("SightMarks40yrds", Float.parseFloat(sightMark40yrds.getText().toString()));
+            myEdit.putFloat("SightMarks50yrds", Float.parseFloat(sightMark50yrds.getText().toString()));
+            myEdit.putFloat("SightMarks60yrds", Float.parseFloat(sightMark60yrds.getText().toString()));
+            myEdit.putFloat("SightMarks80yrds", Float.parseFloat(sightMark80yrds.getText().toString()));
+            myEdit.putFloat("SightMarks100yrds", Float.parseFloat(sightMark100yrds.getText().toString()));
+
+            myEdit.putFloat("SightMarks30mtrs", Float.parseFloat(sightMark30mtrs.getText().toString()));
+            myEdit.putFloat("SightMarks50mtrs", Float.parseFloat(sightMark50mtrs.getText().toString()));
+            myEdit.putFloat("SightMarks70mtrs", Float.parseFloat(sightMark70mtrs.getText().toString()));
+            myEdit.putFloat("SightMarks90mtrs", Float.parseFloat(sightMark90mtrs.getText().toString()));
+
+        } catch (NumberFormatException nfe){
+            myEdit.putInt("Poundage", 0);
+
+            myEdit.putFloat("BracingHeight",0F);
+
+            myEdit.putFloat("SightMarks20yrds",0F);
+        }
+
+
+        myEdit.apply();
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sp = getSharedPreferences("SharedPreferences", MODE_PRIVATE);
+        String BowTypeString = sp.getString("BowType","");
+        int poundageInt = sp.getInt("Poundage", 0);
+
+        float bracingHeightFloat = sp.getFloat("BracingHeight",0F);
+
+        float sightMarks20yrdsFloat = sp.getFloat("SightMarks20yrds", 0F);
+        float sightMarks30yrdsFloat = sp.getFloat("SightMarks30yrds", 0F);
+        float sightMarks40yrdsFloat = sp.getFloat("SightMarks40yrds", 0F);
+        float sightMarks50yrdsFloat = sp.getFloat("SightMarks50yrds", 0F);
+        float sightMarks60yrdsFloat = sp.getFloat("SightMarks60yrds", 0F);
+        float sightMarks80yrdsFloat = sp.getFloat("SightMarks80yrds", 0F);
+        float sightMarks100yrdsFloat = sp.getFloat("SightMarks100yrds", 0F);
+
+        float sightMarks30mtrsFloat = sp.getFloat("SightMarks30mtrs", 0F);
+        float sightMarks50mtrsFloat = sp.getFloat("SightMarks50mtrs", 0F);
+        float sightMarks70mtrsFloat = sp.getFloat("SightMarks70mtrs", 0F);
+        float sightMarks90mtrsFloat = sp.getFloat("SightMarks90mtrs", 0F);
+
+        bowType.setText(BowTypeString);
+        poundage.setText(String.valueOf(poundageInt));
+
+        bracingHeight.setText(String.valueOf(bracingHeightFloat));
+
+        sightMark20yrds.setText(String.valueOf(sightMarks20yrdsFloat));
+        sightMark30yrds.setText(String.valueOf(sightMarks30yrdsFloat));
+        sightMark40yrds.setText(String.valueOf(sightMarks40yrdsFloat));
+        sightMark50yrds.setText(String.valueOf(sightMarks50yrdsFloat));
+        sightMark60yrds.setText(String.valueOf(sightMarks60yrdsFloat));
+        sightMark80yrds.setText(String.valueOf(sightMarks80yrdsFloat));
+        sightMark100yrds.setText(String.valueOf(sightMarks100yrdsFloat));
+
+        sightMark30mtrs.setText(String.valueOf(sightMarks30mtrsFloat));
+        sightMark50mtrs.setText(String.valueOf(sightMarks50mtrsFloat));
+        sightMark70mtrs.setText(String.valueOf(sightMarks70mtrsFloat));
+        sightMark90mtrs.setText(String.valueOf(sightMarks90mtrsFloat));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        savePreferences();
     }
 }
